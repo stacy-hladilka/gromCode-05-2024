fb0415ec36146b053f1e647f51d379fcd599d825
stacy-hladilka <NastiaKlein2049@gmail.com>
2024-06-18 22:38:45 +0300

    add dom task2 and task3

---
+++ DOM/task2/index.js
@@ -0,0 +1,11 @@
+export const getTitleElement = () => {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+};
+
+export const getInputElement = () => {
+  const inputElem = document.querySelector('input[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
+};
---
+++ DOM/task3/index.js
@@ -0,0 +1,12 @@
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll(".technology");
+  console.dir(elementsList);
+  return elementsList;
+};
+
+export const getItemsArray = () => {
+  const NodeList = document.querySelectorAll(".tool");
+  const elementsArray = Array.from(NodeList);
+  console.dir(elementsArray);
+  return elementsArray;
+};
